af1242bdfa33bbc3b660c3eacd255a25d2cb80d8
Bhargav gopal <jbhargavagopal@gmail.com>
2026-02-22 13:42:09 +0530

    dynamic port for deployment

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get("/game", (req,res) => {
     res.sendFile(__dirname + "/game.html");
 });
 
-app.listen(3000, ()=> {
-    console.log("Server running on port 3000.");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
